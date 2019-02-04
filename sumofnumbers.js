2377bce0bda94264ec19eb8418224333c61e0b23
Applemanner <appplemouth@gmail.com>
2019-02-03 23:01:42 -1000

    22:36 Attempt 1

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,7 +1,7 @@
 const testNums = [1,2,3,4];
 function sumFor(nums) {
   let result = 0;
-  for (let i = 0; i <= nums.length; i++) {
+  for (let i = 0; i <= nums.length - 1; i++) {
     result += num;
   }
   return result;
